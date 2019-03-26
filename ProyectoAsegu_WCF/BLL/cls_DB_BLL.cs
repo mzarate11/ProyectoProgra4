6966e0044a3db8873af3b6448cb36fa224877b26
Manuel <Manuel@MANUEL-PC>
2019-03-26 13:00:14 -0600

    Primer commit

--- ProyectoAsegu_WCF/BLL/cls_DB_BLL.cs
+++ ProyectoAsegu_WCF/BLL/cls_DB_BLL.cs
@@ -3,12 +3,93 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 using DAL;
 
 namespace BLL
 {
     public class cls_DB_BLL
     {
+        public void CrearDtParametros(ref cls_DB_DAL Obj_DAL)
+        {
+            DataColumn dcNombre = new DataColumn(@"Nombre", typeof(string));
+            DataColumn dcTipoDato = new DataColumn(@"TipoDato", typeof(string));
+            DataColumn dcValor = new DataColumn(@"Valor", typeof(string));
+
+            Obj_DAL.dtParametros.Columns.Add(dcNombre);
+            Obj_DAL.dtParametros.Columns.Add(dcTipoDato);
+            Obj_DAL.dtParametros.Columns.Add(dcValor);
+        }
+
+        public void Traer_cnx(ref cls_DB_DAL ObjDB_DAL)
+        {
+            try
+            {
+                ObjDB_DAL.sCadena = ConfigurationManager.ConnectionStrings[].ToString().Trim();
+
+                ObjDB_DAL.sql_CNX = new SqlConnection(ObjDB_DAL.sCadena);
+
+                ObjDB_DAL.sMsgError = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                ObjDB_DAL.sCadena = string.Empty;
+                ObjDB_DAL.sql_CNX = null;
+                ObjDB_DAL.sMsgError = ex.Message.ToString();
+            }
+
+        }
+
+        public void Abrir_cnx(ref cls_DB_DAL ObjDB_DAL)
+        {
+            try
+            {
+                Traer_cnx(ref ObjDB_DAL);
+                if (ObjDB_DAL.sql_CNX != null)
+                {
+                    if(ObjDB_DAL.sql_CNX.State == ConnectionState.Closed)
+                    {
+                        ObjDB_DAL.sql_CNX.Open();
+                    }
+                    ObjDB_DAL.sMsgError = string.Empty;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ObjDB_DAL.sCadena = string.Empty;
+                ObjDB_DAL.sql_CNX = null;
+                ObjDB_DAL.sMsgError = ex.Message.ToString();
+            }
+        }
+
+        public void Cerrar_cnx(ref cls_DB_DAL ObjDB_DAL)
+        {
+            try
+            {
+                Traer_cnx(ref ObjDB_DAL);
+                if (ObjDB_DAL.sql_CNX != null)
+                {
+                    if (ObjDB_DAL.sql_CNX.State == ConnectionState.Open)
+                    {
+                        ObjDB_DAL.sql_CNX.Close();
+                    }
+                    ObjDB_DAL.sMsgError = string.Empty;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ObjDB_DAL.sCadena = string.Empty;
+                ObjDB_DAL.sql_CNX = null;
+                ObjDB_DAL.sMsgError = ex.Message.ToString();
+            }
+
+        }
+
 
     }
 }
--- ProyectoAsegu_WCF/DAL/cls_DB_DAL.cs
+++ ProyectoAsegu_WCF/DAL/cls_DB_DAL.cs
@@ -3,11 +3,123 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace DAL
 {
     public class cls_DB_DAL
     {
+        private string _sCadena, _sSentencia, _sNombreTabla, _sMsgError;
 
+        private DataTable _dtParametros = new DataTable("Parametros");
+
+        private SqlConnection _sql_CNX;
+        private SqlDataAdapter _sql_DA;
+        private DataSet _DS;
+
+        public string sCadena
+        {
+            get
+            {
+                return _sCadena;
+            }
+
+            set
+            {
+                _sCadena = value;
+            }
+        }
+
+        public string sSentencia
+        {
+            get
+            {
+                return _sSentencia;
+            }
+
+            set
+            {
+                _sSentencia = value;
+            }
+        }
+
+        public string sNombreTabla
+        {
+            get
+            {
+                return _sNombreTabla;
+            }
+
+            set
+            {
+                _sNombreTabla = value;
+            }
+        }
+
+        public string sMsgError
+        {
+            get
+            {
+                return _sMsgError;
+            }
+
+            set
+            {
+                _sMsgError = value;
+            }
+        }
+
+        public DataTable dtParametros
+        {
+            get
+            {
+                return _dtParametros;
+            }
+
+            set
+            {
+                _dtParametros = value;
+            }
+        }
+
+        public SqlConnection sql_CNX
+        {
+            get
+            {
+                return _sql_CNX;
+            }
+
+            set
+            {
+                _sql_CNX = value;
+            }
+        }
+
+        public SqlDataAdapter sql_DA
+        {
+            get
+            {
+                return _sql_DA;
+            }
+
+            set
+            {
+                _sql_DA = value;
+            }
+        }
+
+        public DataSet DS
+        {
+            get
+            {
+                return _DS;
+            }
+
+            set
+            {
+                _DS = value;
+            }
+        }
     }
 }

dcc649f4a6a22fc03d1bb7abe6c1d4b4819e6092
Laboratorio <Laboratorio@UAM-LB2-06>
2019-04-09 18:23:09 -0600

    Cammbio1

--- PolizaUI/PolizaUI/Paginas/Administrativo/TarjetaCreditoDeb.aspx.designer.cs
+++ PolizaUI/PolizaUI/Paginas/Administrativo/TarjetaCreditoDeb.aspx.designer.cs
@@ -28,6 +28,6 @@ namespace PolizaUI.Paginas {
         /// Campo generado automáticamente.
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GV;
+        //protected global::System.Web.UI.WebControls.GridView GV;
     }
 }

ad0c317adaaeee069c86db0cc1dd28bc1c1784f4
Laboratorio <Laboratorio@UAM-LB2-06>
2019-04-09 20:23:42 -0600

    commint

--- PolizaUI/PolizaUI/Paginas/Cliente/Poliza.aspx.designer.cs
+++ PolizaUI/PolizaUI/Paginas/Cliente/Poliza.aspx.designer.cs
@@ -11,14 +11,5 @@ namespace PolizaUI.Paginas {
     
     
     public partial class Persona {
-        
-        /// <summary>
-        /// btn_A control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlInputButton btn_A;
     }
 }

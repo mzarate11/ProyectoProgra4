eaf821b51f58b51eb404b6cc5686e9a8a829b7fa
Alejandro <ale_sa-vi@hotmail.com>
2019-04-13 08:48:58 -0600

    Creacion de DAL y BLL de Distritos Para el  uso de los SP's de la base de datos y ver/modificar/eliminar/update archivos

---
+++ PolizaUI/BLL/BLL_ADMIN/cls_Distrito_BLL.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.DAL_ADMIN;
+using System.Configuration;
+
+namespace BLL.BLL_ADMIN
+{
+    class cls_Distrito_BLL
+    {
+        public void Listar_Distritos(ref cls_Distrito_DAL Obj_DAL, string sMsjError)
+        {
+            Svc_DataBase.I_DBClient ObjSVC = new Svc_DataBase.I_DBClient();
+            Obj_DAL.DS1 = new System.Data.DataSet();
+            Obj_DAL.sNombre_sp = ConfigurationManager.AppSettings["Listar_Distritos"].ToString();
+            Obj_DAL.DS1.Tables.Add(ObjSVC.ListarDatos(Obj_DAL.sNombre_sp, ref sMsjError));
+        }
+    }
+}
